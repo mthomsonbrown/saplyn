388c4000c18072464315c1de984c3ec3a2b248e6
mthomsonbrown <mthomsonbrown@gmail.com>
2016-10-14 03:37:00 +0000

    Nested the user output json in a user object in order to conform with the expected result from the Java side.  In the end, I think this makes things consistent between input and output, but feels slightly hackish in that I can't just send user, I need to send { user: user }

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,23 +3,23 @@ class Api::V1::UsersController < ApplicationController
   skip_before_filter :authenticate_user!
   skip_before_filter :verify_authenticity_token
   
-  rescue_from ActiveRecord::RecordInvalid do
-    render :text => 'Ya done screwed up', :success => false, :status => :unprocessable_entity
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: {error: {RecordInvalid: e.record.errors}}, status: 422
   end
   
   # POST /users.json
   def create
-    @user = User.create! user_params
-    render json: { data: { auth_token: @user.auth_token, user_id: @user.id }}
+    user = User.create! user_params
+    render :json =>  {user: user}
   end
   
   def sign_in
     user = User.find_by(email: user_params[:email])
-      if user && user.password == user_params[:password]
-        render :json => user
-      else
-        render json: { error: 'Incorrect credentials' }, status: 401
-      end
+    if user && user.valid_password?(user_params[:password])
+      render :json => {user: user}
+    else
+      render json: {error: 'Incorrect credentials'}, status: 401
+    end
   end
   
   def index 
